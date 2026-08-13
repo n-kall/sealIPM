2ee144e78f896189b1dd105fa05eabaf8c89b38b
Kallioinen Noa <33577035+n-kall@users.noreply.github.com>
2026-08-13 13:13:20 +0300

    update logo

--- inst/logo.R
+++ inst/logo.R
@@ -2,23 +2,19 @@ library(hexSticker)
 
 sticker(
     subplot = "seal.pdf",
-
     s_x = 1.00,
     s_y = 1.1,
     s_width = 0.45,
-
     package = "sealIPM",
     p_x = 1.00,
     p_y = 0.5,
-    p_size = 20,
+    p_size = 7,
     p_color = "black",
     p_family = "mono",
     h_fill = "white",
     h_color = "#08263B",
     h_size = 2.2,
-
     url = "",
     u_size = 0,
-
     filename = "sealIPM.svg"
 )
